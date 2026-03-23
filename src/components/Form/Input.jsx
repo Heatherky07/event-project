28b06b35b08c83794cf8331ea56da6642e6b7365
Heatherky07 <r2023028631@feuroosevelt.edu.ph>
2026-03-23 09:27:33 +0800

    Sign-up/Input

---
+++ src/components/Form/Input.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Input = ({label,type, placeholder, name}) => {
+  return (
+    <fieldset className="fieldset">
+    <legend className="fieldset-legend">{label}</legend>
+        <input 
+        name={name} 
+        type={type} 
+        className="input" 
+        placeholder={placeholder} />
+        </fieldset>
+  )
+}
+
+export default Input
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,34 @@
+import Input from '../components/Form/Input';
 import MainLayout from '../Layouts/MainLayout';
 const SignUp = () => {
  return (
-    <MainLayout>SignUp to</MainLayout>
+    <MainLayout>
+      <Input 
+      label="Firstname" 
+       name="Firstname" 
+      placeholder="Enter your First Name"
+      type= "text" 
+      />
+       <Input 
+      label="Lastname" 
+       name="Lasttname" 
+      placeholder="Enter your Last Name" 
+      type= "text" 
+      />
+       <Input 
+      label="Email" 
+       name="Email" 
+      placeholder="Enter your Email" 
+      type= "email" 
+      />
+       <Input 
+      label="Password" 
+       name="Password" 
+      placeholder="Enter your Password" 
+      type= "password" 
+      />
+    </MainLayout>
   );
 };
 
-export default SignUp
+export default SignUp;
