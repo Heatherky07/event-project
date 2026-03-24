05a4b4d3da88b48d111ee448bb99319ad561ed8b
Heatherky07 <r2023028631@feuroosevelt.edu.ph>
2026-03-24 21:22:00 +0800

    Edit Profile to Profile

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,12 +1,86 @@
 import MainLayout from "../Layouts/MainLayout"
+import { useContext, useEffect } from "react";
+import { SessionContext } from "../Contexts/SessionContext";
+import { supabase } from "../utils/supabase";
+import { Link } from "react-router";
+import Card from "../components/Card";
+import Input from "../components/Form/Input";
+import CheckIcon from "../components/Icons/CheckIcon";
+import { useNavigate } from "react-router";
+
+
 
 const Edit = () => {
+    const session = useContext(SessionContext);
+    const navigate = useNavigate();
+
+    useEffect(() => { }, [session, navigate])
+    if (session) {
+        navigate("/Profile");
+    }
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const signupForm = {
+            firstname: formData.get("firstname"),
+            lastname: formData.get("lastname"),
+            email: formData.get("email"),
+            password: formData.get("password"),
+        };
+
+        const { data: profileData, error: profileError } = await supabase
+            .from("profiles")
+            .update({
+                firstname: signupForm.firstname,
+                lastname: signupForm.lastname,
+                email: signupForm.email,
+            })
+            .eq("id", session.user.id)
+            .select();
+
+        if (profileError) alert(profileError);
+        if (profileData) {
+            navigate("/profile");
+        }
+    };
+
     return (
         <MainLayout>
-            <div className="text-center py-24 -px-8">
+            <div className="min-h-screen flex flex-col">
+                <div className="flex justify-center items-center flex-1">
+                    <Card>
+                        <h1 className="text-xl font-bold">Edit Profile</h1>
+                        <form onSubmit={handleSubmit}>
+                            <Input
+                                name="firstname"
+                                placeholder="Enter your First Name"
+                                label="Firstname"
+                                type="text"
+                            />
+                            <Input
+                                name="lastname"
+                                placeholder="Enter your Last Name"
+                                label="Lastname"
+                                type="text"
+                            />
+                            <Input
+                                name="email"
+                                placeholder="Enter your Email"
+                                label="Email"
+                                type="email"
+                            />
+                            <button className="btn btn-primary rounded-full mt-5">
+                                <CheckIcon className="text-sm" /> Submit
+                            </button>
+                        </form>
+                    </Card>
+                </div>
+            </div>
         </MainLayout>
-        >
-    )
-}
 
-export default Edit
+    );
+};
+
+export default Edit;
